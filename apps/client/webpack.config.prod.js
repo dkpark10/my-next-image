f4655419555cad19a869afa7cba6def3e62c2801
dkpark10 <dkpark10@naver.com>
2024-09-22 21:05:15 +0900

    chore: webpack target 분리

--- apps/client/webpack.config.prod.js
+++ apps/client/webpack.config.prod.js
@@ -4,12 +4,20 @@ const common = require('./webpack.config.common');
 
 const { merge } = require('webpack-merge');
 
-module.exports = merge(common, {
-  mode: 'production',
+module.exports = (env) => {
+  const target = env.target;
 
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
-    // publicPath: '/' 경로 prefix
-  },
-});
+  if (target !== 'node' && target !== 'web') {
+    throw new Error('target은 node나 web두가지 값을 설정하여야 합니다.');
+  }
+
+  return merge(common, {
+    mode: 'production',
+
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].[contenthash].js',
+      // publicPath: '/' 경로 prefix
+    },
+  });
+};
