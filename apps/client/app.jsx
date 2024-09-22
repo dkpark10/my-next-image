ec06d5b79bcf2083d75c33a8b7859ae8febaa692
dkpark10 <dkpark10@naver.com>
2024-09-22 22:16:20 +0900

    feat: 간단한 페이지 라우팅 추가

--- apps/client/app.jsx
+++ apps/client/app.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { Suspense } from 'react';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+
+function Post() {
+  return <div>post</div>
+}
+
+function Viewer() {
+  return <div>viewer</div>
+}
 
 export default function App() {
   return (
-    <React.Fragment>
-      <div>123</div>
-    </React.Fragment>
+    <BrowserRouter>
+      <nav>
+        <Link to='/'>홈으로</Link>
+        <Link to='/post'>포스트로</Link>
+        <Link to='/viewer'>뷰어로</Link>
+      </nav>
+      <Suspense fallback={<div>...loading...</div>}>
+        <Routes>
+          <Route path='/' element={<div>home</div>} />
+          <Route path='/post' element={<Post />} />
+          <Route path='/viewer' element={<Viewer />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 }
