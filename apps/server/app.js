12c0d04cf45d44654c707c763c935295ef481384
dkpark10 <dkpark10@naver.com>
2024-09-23 16:25:28 +0900

    feat: ssr 임시 구현

--- apps/server/app.js
+++ apps/server/app.js
@@ -1,14 +1,48 @@
-const express = require('express');
+import express from 'express';
+import path from 'path';
+
 const app = express();
 const port = 8080;
-const { ChunkExtractor } = require('@loadable/server');
 
-const apiRouter = require('./router/api.controller');
+import { ChunkExtractor } from '@loadable/server';
+import { renderToString } from 'react-dom/server';
 
-app.use('/api', apiRouter);
+// const apiRouter = require('./router/api.controller');
+// app.use('/api', apiRouter);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const nodeStats = path.resolve(
+  __dirname,
+  '../../resources/dist/node/loadable-stats.json'
+);
+
+const webStats = path.resolve(
+  __dirname,
+  '../../resources/dist/web/loadable-stats.json'
+);
+
+app.get('/', (_req, res) => {
+  const nodeExtractor = new ChunkExtractor({ statsFile: nodeStats });
+  const { default: App } = nodeExtractor.requireEntrypoint();
+
+  const webExtractor = new ChunkExtractor({ statsFile: webStats });
+  const jsx = webExtractor.collectChunks(<App />);
+
+  const html = renderToString(jsx);
+
+  res.set('content-type', 'text/html');
+  res.send(`
+      <!DOCTYPE html>
+      <html>
+        <head>
+        ${webExtractor.getLinkTags()}
+        ${webExtractor.getStyleTags()}
+        </head>
+        <body>
+          <div id="main">${html}</div>
+          ${webExtractor.getScriptTags()}
+        </body>
+      </html>
+    `);
 });
 
 app.listen(port, () => {
